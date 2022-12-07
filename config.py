a206a756e12f3d85e0ce229c49e2e2007bcd4a9d
Christian <cflorea@uwaterloo.ca>
2022-12-06 21:40:08 -0500

    Created SwimRankingComms class with basic methods

---
+++ config.py
@@ -0,0 +1,48 @@
+teams = [
+    "McMaster University",
+    "University of Toronto",
+    "University of Western Ontario",
+    "University of Waterloo"
+]
+
+relayAliases = {
+    "MACU": "McMaster University",
+    "UT": "University of Toronto",
+    "Mustangs": "University of Western Ontario",
+    "Warriors": "University of Waterloo"
+}
+
+aliases = {
+    "MACU": "McMaster University",
+    "UT": "University of Toronto",
+    "WES": "University of Western Ontario",
+    "WAT": "University of Waterloo"
+}
+
+swimming_event_id = {
+    "50 Free": 1,
+    "100 Free": 2,
+    "200 Free": 3,
+    "400 Free": 5,
+    "800 Free": 6,
+    "1500 Free": 8,
+}
+
+oua_points = {
+    1: 20,
+    2: 17,
+    3: 16,
+    4: 15,
+    5: 14,
+    6: 13,
+    7: 12,
+    8: 11,
+    9: 9,
+    10: 7,
+    11: 6,
+    12: 5,
+    13: 4,
+    14: 3,
+    15: 2,
+    16: 1
+}
--- main.py
+++ main.py
@@ -2,10 +2,90 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import string
+import config
+
+
+class SwimRankingComms:
+    """
+    Get data from swinrankings.net using Beautifulsoup library
+    """
+
+    def __init__(self, meet_id: str) -> None:
+        self.meet_id = meet_id
+        url = f"https://www.swimrankings.net/index.php?page=meetDetail&meetId={meet_id}&gender=1&styleId=0"
+
+    @staticmethod
+    def reformat_soup(soup) -> list:
+        """"""
+        result = []
+        translator = str.maketrans("", "", string.punctuation)
+
+        for item in soup.next_elements:
+            # print(item)
+            # print("\n")
+            # Remove leading spaces
+            if str(item)[0] == " ":
+                item = item[1:]
+            # Remove html tags
+            if str(item)[-8:] == "</table>":
+                break
+            if str(item)[0] == "<":
+                continue
+            if str(item)[:9] == "GENERATED":
+                break
+            # Remove punctuation
+            item = item.translate(translator)
+            # Add team name to list
+            result.append(item)
+        return result
+
+    def get_team_names(self) -> list:
+        result = []
+        # Meet home page
+        page = requests.get(
+            f"https://www.swimrankings.net/index.php?page=meetDetail&meetId={self.meet_id}&gender=1&styleId=0")
+        # Scale website
+        soup = BeautifulSoup(page.content, 'html.parser')
+        teams = soup.find("table").find(
+            class_="meetResult0").find(class_="club")
+        teams = self.reformat_soup(teams)
+        # Return team names
+        result = teams[::7]
+        return result
+
+    def get_team_points_by_event(self) -> dict:
+        teams_in_meet = self.get_team_names()
+        result = {team: [0, 0, 0] for team in teams_in_meet}
+        for gender in range(1, 3):
+            # print(f"gender: {gender}")
+            for id in config.swimming_event_id.values():
+                # print(f"id: {id}")
+                page = requests.get(
+                    f"https://www.swimrankings.net/index.php?page=meetDetail&meetId={self.meet_id}&gender={gender}&styleId={id}")
+                soup = BeautifulSoup(page.content, 'html.parser')
+                event_results = soup.find("table").find(class_="meetResult0")
+                if event_results != None:
+                    event_results = self.reformat_soup(event_results)
+                else:
+                    continue
+                if "Split" in event_results[::7]:
+                    print("SPLIT")
+                    continue
+                teams_in_event = event_results[4::7]
+                place = 0
+                for team in teams_in_event:
+                    place += 1
+                    if team in teams_in_meet:
+                        if place in config.oua_points.keys():
+                            # print(
+                            #     f"Adding {config.oua_points[place]} points to {team}")
+                            result[team][0] += config.oua_points[place]
+                            result[team][gender] += config.oua_points[place]
+        return result
 
 
 page = requests.get(
-    "https://www.swimrankings.net/index.php?page=meetDetail&meetId=629800&gender=1&styleId=17")
+    "https://www.swimrankings.net/index.php?page=meetDetail&meetId=634465&gender=1&styleId=17")
 soup = BeautifulSoup(page.content, 'html.parser')
 result_header = soup.find(class_="meetResultHead")
 
@@ -43,29 +123,34 @@ for element in event0.next_elements:
         continue
 
 for result in results:
-    print(result)
+    pass
+    # print(result)
 
+    # event0 = soup.find_all(class_="meetResult0")
+    # print(len(event0))
 
-# event0 = soup.find_all(class_="meetResult0")
-# print(len(event0))
+    # tags = {tag.name for tag in event0}
 
-# tags = {tag.name for tag in event0}
+    # print(len(tags))
 
-# print(len(tags))
+    # for tag in tags:
+    #     print(tag)
 
-# for tag in tags:
-#     print(tag)
+    # for string in event0:
+    #     if contains("name", str(string)):
+    #         filtered_menu.append(string)
 
-# for string in event0:
-#     if contains("name", str(string)):
-#         filtered_menu.append(string)
+    # print(filtered_menu[0])
 
-# print(filtered_menu[0])
+    # event_name = event0.find(class_="name").get_text()
+    # print(event_name)
 
-# event_name = event0.find(class_="name").get_text()
-# print(event_name)
+    # event0 = soup.find(class_="meetResult0")
 
-# event0 = soup.find(class_="meetResult0")
+    # event_name = event0.find(class_="name").get_text()
+    # print(event_name)
 
-# event_name = event0.find(class_="name").get_text()
-# print(event_name)
+if __name__ == "__main__":
+    oua = SwimRankingComms(meet_id="629800")
+    print(oua.get_team_names())
+    print(oua.get_team_points_by_event())
